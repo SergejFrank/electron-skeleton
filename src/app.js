3249c3d22ca298b3a77135156cdf6a181a6e216d
Sergej Frank <git@sergej-frank.de>
2017-06-27 14:25:41 +0200

    reorganized

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const {app, BrowserWindow, ipcMain} = require('electron');
 const path = require('path');
 const url = require('url');
-const conf = require('./conf');
+const conf = require('../conf');
 
 let win;
 
